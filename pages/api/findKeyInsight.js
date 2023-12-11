c5d62a84b69817235824942e5716b243ecc2e038
Swizec Teller <swizec@swizec.com>
2023-12-11 09:37:50 -0800

    Fix alternative

--- pages/api/findKeyInsight.js
+++ pages/api/findKeyInsight.js
@@ -1,7 +1,8 @@
 import { getKeyInsight } from "../../aistuff/getKeyInsight";
 
 export default async (req, res) => {
-    const insight = getKeyInsight(title, content);
+    const { title, content } = req.body;
+    const insight = await getKeyInsight(title, content);
 
     res.status(200).send(insight.join("\n\n"));
 };
